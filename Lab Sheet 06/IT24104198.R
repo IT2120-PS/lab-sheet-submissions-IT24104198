7af28c4f9a23c3230a3454d11368d4132ea07961
IT24104198 <it24104198@my.sliit.lk>
2025-09-07 23:25:49 +0530

    Add files to Lab Sheet 06

---
+++ Lab Sheet 06/IT24104198.R
@@ -0,0 +1,10 @@
+setwd("C:\\Users\\dinit\\OneDrive\\Desktop\\Y2S1\\PS")
+
+##Question 01
+##i)Distribution of n = 50 and p = 0.85
+pbinom(47,50,0.85,lower.tail = FALSE)
+##We use false if it's a greater value
+
+##Question 02
+##i)Number of average calls received per hour
+##ii)Poisson Distribution
