e2661c1420697269f868252b274683b6b1943e43
IT24104198 <it24104198@my.sliit.lk>
2025-09-16 12:17:50 -0600

    Add files to Lab Sheet 07

---
+++ Lab Sheet 07/IT24104198.R
@@ -0,0 +1,15 @@
+setwd("C:\\Users\\dinit\\OneDrive\\Desktop\\Y2S1\\PS")
+
+#01
+punif(25, min = 0, max = 40, lower.tail = TRUE) - punif(10, min = 0, max = 40, lower.tail = TRUE)
+
+#02
+pexp(2, rate=1/3,lower.tail = TRUE)
+
+#03
+
+#1
+1 - pnorm(130, mean=100, sd=15, lower.tail = TRUE)
+
+#2
+qnorm(0.95, mean=100, sd=15, lower.tail = TRUE)
